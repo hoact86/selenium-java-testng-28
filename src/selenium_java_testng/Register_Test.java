791086b4453d62c9f7423403e8c2642939099383
Chu Thi Hoa <hoact3@gmail.com>
2023-01-31 20:30:23 +0700

    Add first commit

---
+++ src/selenium_java_testng/Register_Test.java
@@ -0,0 +1,52 @@
+package selenium_java_testng;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Register_Test {
+	WebDriver driver;
+	String projectPath=System.getProperty("user.dir");
+	String osName=System.getProperty("os.name");
+	
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath+"\\browserDriver\\geckodriver.exe");
+		
+		if (osName.contains("Windows")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		} else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		}
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+	}
+	
+	@Test
+	public void TC_01_Empty_Data() {
+		driver.get("https://alada.vn/tai-khoan/dang-ky.html");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		// Verify	
+		Assert.assertEquals(driver.findElement(By.id("txtFirstname-error")).getText(), "Vui lòng nhập họ tên");
+		Assert.assertEquals(driver.findElement(By.id("txtEmail-error")).getText(), "Vui lòng nhập email");
+		Assert.assertEquals(driver.findElement(By.id("txtCEmail-error")).getText(), "Vui lòng nhập lại địa chỉ email");
+		Assert.assertEquals(driver.findElement(By.id("txtPassword-error")).getText(), "Vui lòng nhập mật khẩu");
+		Assert.assertEquals(driver.findElement(By.id("txtCPassword-error")).getText(), "Vui lòng nhập lại mật khẩu");
+		Assert.assertEquals(driver.findElement(By.id("txtPhone-error")).getText(), "Vui lòng nhập số điện thoại.");
+
+	}
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	
+}
---
+++ src/selenium_java_testng/Topic_03_XPath.java
@@ -0,0 +1,19 @@
+package selenium_java_testng;
+
+import org.openqa.selenium.WebDriver;
+
+ 
+
+public class Topic_03_XPath {
+	WebDriver driver;
+	String projectPath=System.getProperty("user.dir");
+	String osName=System.getProperty("os.name");
+
+	public void beforeClase() {
+		
+	}
+	
+	public void TC_01_Empty_Data() {
+		
+	}
+}
